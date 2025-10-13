3a47fafadee9547e94a6f96fbeeb31c6c1d27118
Rafael Slivka <129512938+rafaslivka@users.noreply.github.com>
2025-10-13 19:49:00 -0300

    Create Developer.java

---
+++ Padrões/Developer.java
@@ -0,0 +1,13 @@
+package composite.antipattern;
+
+public class Developer {
+    private String name;
+
+    public Developer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
